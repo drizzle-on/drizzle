778f334b51d81ff26a333264c3167d67237f14c9
Pyry Helkkula <pyry.helkkula@gmail.com>
2016-10-19 15:38:46 +0300

    Remove Y, MT splitmulti into biallelic, write vds

--- src/qc.sh
+++ src/qc.sh
@@ -7,3 +7,8 @@ export SOURCE=
 
 /spark/bin/spark-submit --master spark://IP-address:7077 /hail/build/libs/hail-all-spark.jar --tmpdir .../ \
   importvcf $SOURCE/my.vcf.bgz \
+  # Remove the Y and mitochondrial chromosomes
+  filtervariants expr -c 'v.contig == "Y" || v.contig == "MT" || v.isBiallelic == false' --remove \
+  # Split multi-allelic SNPs into biallelic
+  splitmulti \
+  write -o my.vds
