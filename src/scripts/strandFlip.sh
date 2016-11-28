604b98696e9c536e5878d29e969f96295e3a6fbc
Pyry Helkkula <pyry.helkkula@gmail.com>
2016-11-28 11:12:05 +0200

    Added spacing

--- src/scripts/strandFlip.sh
+++ src/scripts/strandFlip.sh
@@ -11,8 +11,10 @@ $PLINK --bfile $REF_FILE --bmerge $FILE_TO_MERGE --out $TRIAL_MERGE
 
 # Flip the strand for variants identified in the initial merge
 $PLINK --bfile $FILE_TO_MERGE --flip $TRIAL_MISSNP --make-bed --out $FLIPPED
+
 # Second merge attempt to identify variants that have other issues than
 # the inconsistent strand assignment
 $PLINK --bfile $REF_FILE --bmerge $FLIPPED --out $FINAL_MISSNP
+
 # Exclude all the unmerged variants
 $PLINK --bfile $FLIPPED --exclude $FINAL_MISSNP-merge.missnp --make-bed --out $OUT
