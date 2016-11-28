ad5d5ad02097da7ce353d07b336fb775ed8b8518
Pyry Helkkula <pyry.helkkula@gmail.com>
2016-11-28 11:01:14 +0200

    Second merging attempt after strand flip

--- src/scripts/strandFlip.sh
+++ src/scripts/strandFlip.sh
@@ -4,9 +4,13 @@ PLINK=
 REF_FILE= 
 FILE_TO_MERGE=
 TRIAL_MERGE=
+FINAL_MISSNP=
 
 # Initial merging of study file with the reference set
 $PLINK --bfile $REF_FILE --bmerge $FILE_TO_MERGE --out $TRIAL_MERGE
 
 # Flip the strand for variants identified in the initial merge
 $PLINK --bfile $FILE_TO_MERGE --flip $TRIAL_MISSNP --make-bed --out $FLIPPED
+# Second merge attempt to identify variants that have other issues than
+# the inconsistent strand assignment
+$PLINK --bfile $REF_FILE --bmerge $FLIPPED --out $FINAL_MISSNP
