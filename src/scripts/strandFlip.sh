f4547c960b230abdb33b54441e47f732dc14ac55
Pyry Helkkula <pyry.helkkula@gmail.com>
2016-11-28 10:57:20 +0200

    Strand flip of unmerged variants

--- src/scripts/strandFlip.sh
+++ src/scripts/strandFlip.sh
@@ -7,3 +7,6 @@ TRIAL_MERGE=
 
 # Initial merging of study file with the reference set
 $PLINK --bfile $REF_FILE --bmerge $FILE_TO_MERGE --out $TRIAL_MERGE
+
+# Flip the strand for variants identified in the initial merge
+$PLINK --bfile $FILE_TO_MERGE --flip $TRIAL_MISSNP --make-bed --out $FLIPPED
